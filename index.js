7f0399157a8286aa5ae09254bb1e108b5f8e2fa8
M-irfan <fannee0011@gmail.com>
2026-07-08 10:31:30 +0500

    boolean uses

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
-console.log("irfan");
-console.log('pakistan');
-console.log(`this is my village`);
+let online=true;
+let attend=false;
+let forsale=true;
+let isPass=false;
+let enrolled=true;
 
-// window.alert(`this page is hacked`)
-// window.alert(`Accept the cookies`)
-
-// document.getElementById("heading").textContent=`webSite hacKed`;
-// document.getElementById("info").textContent=`you are not programer.`;
-
-// let x=5;
-// let y=x+7
-
-// console.log(`x+y=`,x+y,`AND  x-2=`,x-2);
-// console.log(`x+y=$ ${x+y} and value of x is ${x}`);
-// console.log(typeof x);
-
-let age=20;
-let name='irfan';
-let gpa=3;
-let character="good";
-let fact=true;
-
-console.log(`My name is ${name}
-my age is ${age}
-my gpa is ${gpa}
-my charater is ${character}
-my charater is ${character}
-I am ${fact} man`);
-
-console.log(`
-    types of variable
-                      `)
-
-console.log(typeof name);
-console.log(typeof fact);
-console.log(typeof gpa);
-console.log(typeof age);
-console.log(typeof character);
-
-
-
-
-
+console.log(`Irfan is online on whatapps: ${online}`);
+console.log(`Irfan is attending the class: ${attend}`);
+console.log(`this mobile is available in market: ${online}`);
+console.log(`yara pass his first class: ${isPass}`);
+console.log(`Irfan is enrolled in cs: ${enrolled}`);
 
