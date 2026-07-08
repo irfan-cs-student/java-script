0c3d9853cfaf077c424d39339b08e15b9b34021f
M-irfan <fannee0011@gmail.com>
2026-07-08 13:19:30 +0500

    arithmatic operators, increment/decrement, operator precedences

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-let name="irfan";
-let age=22;
-let isStudent=false;
-let enrolled=true;
+// variable
+
+let a=7;
+console.log(a);
+
+//operators
+
+// a++; 
+a--;
+console.log(a);
+console.log(a++);  // add 1 after print
+console.log(a);
+console.log(a--);    
+console.log(a);
+console.log(a-1);
+console.log(a--);
+console.log(a);
+
+//operators
+let x=3;
+let y=2;
+let z=1;
+
+console.log(x,   y**2   ,z);
+console.log(x-=4,y**4   ,z);
+console.log(x+=4,y-4      ,z+=8);
+console.log(x%=3,y-=8     ,z);
+
+/*impoetan points 
+if we write a+3;its will change value insie console printing 
+but if we add like = with other in event print it will channge actual and future all variable values
+*/
+
+//operator precedences
+
+let abc=2*6/2-3-8*3+(5-3);
+console.log(abc)
+
+/* operators precedence
+   brackets > %,/,* >=-
+   if %/* OR =- or brakets all comes in one line then start soloution from left to right in questin statemnet for their respective operators
+*/
+
+// how upper question is solved:
+// abc=2*6/2-3-8*3+(5-3);
+// 1 step: (5-3)=2
+//          2*6=12
+//          12/2=6
+//          8*3=24
+//          2-24=-22-3=25
+//          -25+6=19
 
 
-document.getElementById("p1").textContent=`my name is :${name}`;
-document.getElementById("p2").textContent=`my age nis  :${age}`;
-document.getElementById("p3").textContent=`student: ${isStudent}`;
-document.getElementById("p4").textContent=`enrolled in bSCS:  ${enrolled}`;
+        
+
+  
+
+
+
+
+
+
