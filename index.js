97617b4a381c5e8e290e7470f87acc50b0806993
M-irfan <fannee0011@gmail.com>
2026-07-08 09:34:53 +0500

    changing text with id

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ console.log("irfan")
 console.log('pakistan')
 console.log(`this is my village`)
 
-window.alert(`this page is hacked`)
-window.alert(`Accept the cookies`)
+// window.alert(`this page is hacked`)
+// window.alert(`Accept the cookies`)
+
+document.getElementById("heading").textContent=`webSite hacKed`;
+document.getElementById(`info`).textContent=`you are not programer.`
