a0be9bc94e9ad114fd909b6d502e368907fb38ec
M-irfan <fannee0011@gmail.com>
2026-07-08 10:05:18 +0500

    printing output in console

--- index.js
+++ index.js
@@ -11,5 +11,19 @@ console.log(`this is my village`)
 let x=5;
 let y=x+7
 
-console.log(`x+y=`,x+y,`AND  x-2=`,x-2)
-console.log(`x+y=$ ${x+y} and value of x is ${x}`)
+console.log(`x+y=`,x+y,`AND  x-2=`,x-2);
+console.log(`x+y=$ ${x+y} and value of x is ${x}`);
+console.log(typeof x);
+
+let age=20;
+let name='irfan';
+let gpa=3;
+let character="good";
+
+console.log(`My name is ${name}
+my age is ${age}
+my gpa is ${gpa}
+my charater is ${character}`);
+
+
+
