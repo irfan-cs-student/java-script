6ceee7146661a7f25457b07ce83a7f53e6d6f3f9
M-irfan <fannee0011@gmail.com>
2026-07-08 09:53:11 +0500

    jawascript variables

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ console.log(`this is my village`)
 // window.alert(`this page is hacked`)
 // window.alert(`Accept the cookies`)
 
-document.getElementById("heading").textContent=`webSite hacKed`;
-document.getElementById(`info`).textContent=`you are not programer.`
+// document.getElementById("heading").textContent=`webSite hacKed`;
+// document.getElementById("info").textContent=`you are not programer.`;
+
+let x=5;
+let y=x+7
+
+console.log(`x+y=`,x+y,`AND  x-2=`,x-2)
+console.log(`x+y=$ ${x+y} and value of x is ${x}`)
