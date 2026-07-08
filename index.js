27de832cf6d8321bb2e34fc0fe374422d4286555
M-irfan <fannee0011@gmail.com>
2026-07-08 10:16:27 +0500

    boolean value

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-console.log("irfan")
-console.log('pakistan')
-console.log(`this is my village`)
+console.log("irfan");
+console.log('pakistan');
+console.log(`this is my village`);
 
 // window.alert(`this page is hacked`)
 // window.alert(`Accept the cookies`)
@@ -8,22 +8,38 @@ console.log(`this is my village`)
 // document.getElementById("heading").textContent=`webSite hacKed`;
 // document.getElementById("info").textContent=`you are not programer.`;
 
-let x=5;
-let y=x+7
+// let x=5;
+// let y=x+7
 
-console.log(`x+y=`,x+y,`AND  x-2=`,x-2);
-console.log(`x+y=$ ${x+y} and value of x is ${x}`);
-console.log(typeof x);
+// console.log(`x+y=`,x+y,`AND  x-2=`,x-2);
+// console.log(`x+y=$ ${x+y} and value of x is ${x}`);
+// console.log(typeof x);
 
 let age=20;
 let name='irfan';
 let gpa=3;
 let character="good";
+let fact=true;
 
 console.log(`My name is ${name}
 my age is ${age}
 my gpa is ${gpa}
-my charater is ${character}`);
+my charater is ${character}
+my charater is ${character}
+I am ${fact} man`);
+
+console.log(`
+    types of variable
+                      `)
+
+console.log(typeof name);
+console.log(typeof fact);
+console.log(typeof gpa);
+console.log(typeof age);
+console.log(typeof character);
+
+
+
 
 
 
