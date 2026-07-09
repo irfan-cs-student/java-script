368313eb09f8c70ed9c64f90e766d60f75337d5a
M-irfan <fannee0011@gmail.com>
2026-07-09 14:29:41 +0500

    bollean true /false

--- type-conversion.js
+++ type-conversion.js
@@ -1,7 +1,29 @@
 
-let age;
-age=window.prompt("whats your age?");
-age=Number(age);
-age+=1;
-console.log(age , typeof age);
+
+let a="cat";
+let b="789";
+let c="";
+ 
+a=Number(a);
+b=String(b);
+c=Boolean(c);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+
+console.log(`---bollean ---`);
+
+console.log(c, typeof c);
+
+    c="abc";
+    c=Boolean(c);
+
+console.log(c, typeof c);
+
+    c=123;
+    c=Boolean(c);
+
+console.log(c, typeof c);
+
+
 
