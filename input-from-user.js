7e005b4f93ab932cbcbb9bd9fcbabeedf0c74b76
M-irfan <fannee0011@gmail.com>
2026-07-09 13:55:19 +0500

    professional way of takin input

--- input-from-user.js
+++ input-from-user.js
@@ -1,7 +1,17 @@
   //  --this is simple way of taking input--
 
-let username=window.prompt("what's yor name is ?");
-console.log(username);
+// let username=window.prompt("what's yor name is ?");
+// console.log(username);
 
+// professional way:
+
+let username;
+document.getElementById("sub-btn").onclick=function(){
+    username=document.getElementById("username").value;
+    console.log(username);
+    document.getElementById("user_name").textContent=`hello, ${username}`;
+
+}
+ 
 
 
